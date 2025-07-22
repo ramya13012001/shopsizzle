67964838adcf2c940424585088d7abbb7d1b7baa
ramya13012001 <ramyataetae@gmail.com>
2025-07-22 21:02:50 +0530

    Ar - first commit

---
+++ Sizzle/src/main/java/com/Shop/Sizzle/SizzleApplication.java
@@ -0,0 +1,13 @@
+package com.Shop.Sizzle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SizzleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SizzleApplication.class, args);
+	}
+
+}
---
+++ Sizzle/src/test/java/com/Shop/Sizzle/SizzleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Shop.Sizzle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SizzleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
